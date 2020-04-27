8dda2b4045524adfabf6421e57fe1f225daf74af
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-04-27 15:29:32 +0530

    favourite tab display  fcn added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,8 @@
-// {
-//     type: 'INCREASE_COUNT',
-// }
-// {
-//     type: 'DECREASE_COUNT',
-// } 
 
 export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
 
 
 export function addMovies (movies){
@@ -20,5 +16,17 @@ export function addFavourite (movie){
     return {
         type: ADD_FAVOURITE,
         movie
+    }
+}
+export function removeFromFavourites (movie){
+    return {
+        type: REMOVE_FROM_FAVOURITES,
+        movie
+    }
+}
+export function setShowFavourites (val){
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import {ADD_MOVIES, ADD_FAVOURITE} from '../actions'
+import {ADD_MOVIES, ADD_FAVOURITE,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES} from '../actions'
 
 const initialMoviesState = {
     list:[],
-    favourites:[]
+    favourites:[],
+    showFavourites: false
 }
 export default function movies (state = initialMoviesState, action) {
     // if(action.type === ADD_MOVIES){
@@ -22,6 +23,20 @@ export default function movies (state = initialMoviesState, action) {
             return{
                 ...state,
                 favourites: [action.movie, ...state.favourites]
+            }
+        case REMOVE_FROM_FAVOURITES:
+            const filteredArray = state.favourites.filter(
+                movie => movie.Title !== action.movie.Title
+            );
+        
+            return{
+                ...state,
+                favourites:filteredArray
+            };
+        case SET_SHOW_FAVOURITES:
+            return{
+                ...state,
+                showFavourites: action.val
             }
         default:
             return state;
