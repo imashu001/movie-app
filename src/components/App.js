b1168526fd3d94c743df35338f66de0dd5739b6b
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-04-27 20:25:11 +0530

    combine reducers function added

--- src/components/App.js
+++ src/components/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
     console.log('STATE',this.props.store.getState());
   }
   isMovieFavourite=(movie)=> {
-    const {favourites} = this.props.store.getState();
-    const index = favourites.indexOf(movie);
+    const {movies} = this.props.store.getState();
+    const index = movies.favourites.indexOf(movie);
     if(index !== -1){
       return true;
     }
@@ -30,7 +30,8 @@ class App extends React.Component {
 
   }
   render(){
-    const {list,favourites,showFavourites} = this.props.store.getState(); //{list[]}, favourites[]
+    const { movies } = this.props.store.getState();
+    const {list,favourites,showFavourites} = movies; //{list[]}, favourites[]
     console.log('RENDER',this.props.store.getState());
     const displayMovies = showFavourites ? favourites : list;
 
