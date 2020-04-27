ede570d0a3558a9be9dadcda79a29b3971a3ef0f
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-04-27 12:07:54 +0530

    actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,13 @@
 // {
 //     type: 'DECREASE_COUNT',
 // } 
+
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+
+export function addMovies (movies){
+    return {
+        type: ADD_MOVIES,
+        movies
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './navbar';
 import MovieCard from './MovieCard';
+import {addMovies} from '../actions';
 
 class App extends React.Component {
   componentDidMount(){
@@ -12,10 +13,7 @@ class App extends React.Component {
       this.forceUpdate();
 
     })
-    this.props.store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
     console.log('STATE',this.props.store.getState());
   }
   render(){
