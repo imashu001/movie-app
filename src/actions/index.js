2b3a1dc78ae969c87190322b866f515a35471e80
imashu001 <60100740+imashu001@users.noreply.github.com>
2020-04-27 14:29:12 +0530

    favourite fcn added and text changes to unfavourire

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,19 @@
 // } 
 
 export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_FAVOURITE = 'ADD_FAVOURITE';
 
 
 export function addMovies (movies){
     return {
         type: ADD_MOVIES,
         movies
+    }
+}
+
+export function addFavourite (movie){
+    return {
+        type: ADD_FAVOURITE,
+        movie
     }
 }
